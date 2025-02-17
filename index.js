d070670b4bdbcc33eab3cab944ef3837f4be5023
Abhilash <abhilash365366@gmail.com>
2025-02-17 20:24:14 +0530

    access

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
 
 
 app.use((req,res,next)=>{
-res.setHeader("Access-Control-Allow-Origin","http://localhost:5173")
+res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173, https://synergic-iitbbs.vercel.app");
 res.header("Access-Control-Allow-Headers","Origin,X-Requested_with,Content-Type,Accept");
 next();
 }) 
