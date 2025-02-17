0fa3573ea1da539d9860f10d852ecaad9139a7d8
Abhilash <abhilash365366@gmail.com>
2025-02-17 20:28:19 +0530

    access

--- index.js
+++ index.js
@@ -34,11 +34,12 @@ app.use((req, res, next) => {
 });
 
 
-app.use((req,res,next)=>{
-res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173, https://synergic-iitbbs.vercel.app");
-res.header("Access-Control-Allow-Headers","Origin,X-Requested_with,Content-Type,Accept");
-next();
-}) 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*"); // Allows all origins
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
 app.use(express.json())
 app.use('/api/',require("./routes/createUser"));
 
